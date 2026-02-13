08d24eee6fa2069813444c4a8a0b6e337b9e5e83
MikeyMictan1 <michaeltansikorski@gmail.com>
2026-02-13 22:17:12 +0000

    Main menu visual revamp complete
    Added background image + airplane animation on loop + button re-naming

--- src/main/java/com/airportsim/AirportSimApp.java
+++ src/main/java/com/airportsim/AirportSimApp.java
@@ -5,8 +5,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.layout.Region;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.animation.TranslateTransition;
+import javafx.util.Duration;
 
 public class AirportSimApp extends Application {
     /** 
@@ -14,52 +19,69 @@ public class AirportSimApp extends Application {
     */
     @Override
     public void start(Stage stage) {
-        // Start button setup
-        Button startButton = new Button("Start Simulation Configuration");
+        // --- BUTTON SETUP ---
+        Button startButton = new Button("Start New Scenario");
         startButton.getStyleClass().add("button");
         startButton.setOnAction(event -> {
             stage.setTitle("Configuration Page"); // CHANGE WITH A STAGE.SETSCENE() CALL TO THE ACTUAL LOAD SCENARIO PAGE ONCE IT'S BUILT
         });
 
-        // Load scenario button setup
-        Button loadScenarioButton = new Button("Load Scenario");
+        Button loadScenarioButton = new Button("Load Previous Scenario");  // Load scenario button setup
         loadScenarioButton.getStyleClass().add("button");
         loadScenarioButton.setOnAction(event -> {
             stage.setTitle("Load Scenario Page"); // CHANGE WITH A STAGE.SETSCENE() CALL TO THE ACTUAL LOAD SCENARIO PAGE ONCE IT'S BUILT
         });
 
-        // Load previous results button setup
-        Button loadResultsButton = new Button("Load Previous Results");
+        
+        Button loadResultsButton = new Button("Load Previous Results"); // Load previous results button setup
         loadResultsButton.getStyleClass().add("button");
         loadResultsButton.setOnAction(event -> {
             stage.setTitle("Load Previous Results Page"); // CHANGE WITH A STAGE.SETSCENE() CALL TO THE ACTUAL LOAD SCENARIO PAGE ONCE IT'S BUILT
         });
-        // Quit button setup
-        Button quitButton = new Button("Quit Simulation");
+        
+        Button quitButton = new Button("Quit Simulation"); // Quit button setup
         quitButton.getStyleClass().add("quit-button");
         quitButton.setOnAction(event -> {
             stage.close();
         });
 
-        // Setting all buttons to have consistent widths
-        double buttonWidth = 350;
+        double buttonWidth = 350; // Setting all buttons to have consistent widths
         startButton.setPrefWidth(buttonWidth);
         loadScenarioButton.setPrefWidth(buttonWidth);
         loadResultsButton.setPrefWidth(buttonWidth);
         quitButton.setPrefWidth(buttonWidth);
   
         // Title text
-        Label titleLabel = new Label("Airport Traffic Simulation");
+        Label titleLabel = new Label("Airport Traffic Studio");
         titleLabel.getStyleClass().add("title");
         
         // Standard Vbox for the buttons and title aligned in the middle 
         Region spacer = new Region();
-        spacer.setMinHeight(100);
+        spacer.setMinHeight(60);
         VBox layout = new VBox(40, titleLabel, spacer, startButton, loadScenarioButton, loadResultsButton, quitButton);
-        layout.getStyleClass().add("container");
+        layout.setStyle("-fx-alignment: center; -fx-padding: 40;");
+
+        // Creating the plane animation 
+        Image planeImage = new Image(getClass().getResource("/images/plane.png").toExternalForm());
+        ImageView planeView = new ImageView(planeImage);
+        planeView.setFitWidth(800);
+        planeView.setPreserveRatio(true);
+        
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(10), planeView);
+        transition.setFromX(-530);      // Start off-screen left
+        transition.setFromY(720);       // Start off-screen bottom
+        transition.setToX(470);        // End off-screen right
+        transition.setToY(-600);        // End off-screen top
+        transition.setCycleCount(TranslateTransition.INDEFINITE); // Loop forever
+        transition.play();
+
+        // Create a StackPane to create the layer order : plane then layout over it
+        StackPane root = new StackPane();
+        root.getStyleClass().add("container-main-menu");
+        root.getChildren().addAll(planeView, layout);
 
         // Creating the actual window (1280/720) and applying the CSS
-        Scene entryScene = new Scene(layout, 1280, 720);
+        Scene entryScene = new Scene(root, 1280, 720);
         entryScene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm()); // apply css
 
         // Showing the actual window
